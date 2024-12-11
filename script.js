18937ad877038457c7657c9dabf84699b0a6c880
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-11 10:08:29 -0300

    com26

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 /* alert("TESTEEEEEEEEEEEEEEE"); */
 
 const form = document.getElementsById("form");
+const username = document.getElementsById("username");
+const email = document.getElementsById("email");
+const password = document.getElementsById("password");
+const passwordConfirmation = document.getElementsById("password-confirmation");
