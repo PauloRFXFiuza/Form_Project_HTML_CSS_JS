7e39ea483dfd9bfcab1bb186e0c30c9bfb5d7e05
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-11 10:10:49 -0300

    com27

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const username = document.getElementsById("username");
 const email = document.getElementsById("email");
 const password = document.getElementsById("password");
 const passwordConfirmation = document.getElementsById("password-confirmation");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    alert("Cadastrado com Sucesso.")
+})
